8ef4709a7cf3fcd8b721f53aa41e524f3235df85
Christopher Schmidt <crschmidt@crschmidt.net>
2011-08-28 12:21:30 -0400

    Changes to models.

--- main/models.py
+++ main/models.py
@@ -9,8 +9,13 @@ class Paddocks(models.Model):
     owner = models.CharField(max_length=50)
     acres = models.FloatField()
     date_added = models.CharField(max_length=25)
-    notes = models.CharField(max_length=10)
-    links = models.CharField(max_length=10)
+    notes = models.CharField(max_length=250)
+    links = models.CharField(max_length=100)
+    photo_vid_links = models.CharField(max_length=100)
+    crop_type = models.CharField(max_length=50)
+    pm_success = models.CharField(max_length=100)
+    pm_problem = models.CharField(max_length=100)
+    pm_plans = models.CharField(max_length=100) 
     the_geom = models.MultiPolygonField(srid=-1)
     objects = models.GeoManager()
     class Meta:
@@ -23,10 +28,32 @@ class Properties(models.Model):
     owner = models.CharField(max_length=50)
     acres = models.FloatField()
     date_added = models.CharField(max_length=25)
-    notes = models.CharField(max_length=10)
-    links = models.CharField(max_length=10)
+    notes = models.CharField(max_length=250)
+    links = models.CharField(max_length=100)
+    photo_vid_links = models.CharField(max_length=100)
+    wcm_success = models.CharField(max_length=100)
+    wcm_problem = models.CharField(max_length=100)
+    wcm_plans = models.CharField(max_length=100)
     the_geom = models.MultiPolygonField(srid=-1)
     objects = models.GeoManager()
     class Meta:
         db_table = u'properties'
+
+class Cells(models.Model):
+    gid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    number = models.FloatField()
+    owner = models.CharField(max_length=50)
+    acres = models.FloatField()
+    date_added = models.CharField(max_length=25)
+    notes = models.CharField(max_length=250)
+    links = models.CharField(max_length=100)
+    photo_vid_links = models.CharField(max_length=100)
+    wcm_success = models.CharField(max_length=100)
+    wcm_problem = models.CharField(max_length=100)
+    wcm_plans = models.CharField(max_length=100)
+    the_geom = models.MultiPolygonField(srid=-1)
+    objects = models.GeoManager()
+    class Meta:
+        db_table = u'cells'
 
