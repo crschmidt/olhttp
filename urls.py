3b9caec4c345bbf7b37ab23fbd5c3ef346152b2c
Christopher Schmidt <crschmidt@crschmidt.net>
2011-09-15 07:43:36 -0600

    Add static paths explicitly in urls.py for local hacking.

--- urls.py
+++ urls.py
@@ -13,4 +13,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+                {'document_root': 'main/static'}),
 )
